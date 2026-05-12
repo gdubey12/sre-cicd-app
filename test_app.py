bd89f06fb5e3589e79870c6975b3c33e5e4789ad
gdubey12 <gauravdb24@gmail.com>
2026-05-12 04:31:23 +0000

    break lab: intentional test failure to trigger notification

--- test_app.py
+++ test_app.py
@@ -11,8 +11,8 @@ def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
     data = response.get_json()
-    assert data['message'] == 'Hello from SRE Labs'  # ← intentional bug
-
+    #assert data['message'] == 'Hello from SRE Labs'  # ← intentional bug
+    assert data['message'] == 'THIS WILL BREAK'  # ← intentional bug
 def test_health(client):
     response = client.get('/health')
     assert response.status_code == 200
